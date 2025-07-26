7ab39cbd5d5dbdbee758cedd6273a3215b5b007f
Matija Misan <matija.misan@outlook.com>
2025-07-26 18:26:13 +0200

    templatetags

---
+++ NKSS/nkss/main/templatetags/form_tags.py
@@ -0,0 +1,13 @@
+from django import template
+from django.forms.widgets import CheckboxSelectMultiple
+
+
+register = template.Library()
+
+@register.filter(name='add_class')
+def add_class(field, css_class):
+    return field.as_widget(attrs={"class": css_class})
+
+@register.filter
+def is_checkboxselectmultiple(field):
+    return isinstance(field.field.widget, CheckboxSelectMultiple)
